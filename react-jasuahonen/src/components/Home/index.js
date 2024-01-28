7b8115690f9d603a7ad99fcb5b079d33beacb87a
Jasu Ahonen <jasu.ahonen@outlook.com>
2024-01-28 15:57:00 +0200

    added Home

--- react-jasuahonen/src/components/Home/index.js
+++ react-jasuahonen/src/components/Home/index.js
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo_j2.png';
+import './index.scss';
+
 const Home = () => {
 
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Home</h1>
+                <h1>Hi, <br /> I'm
+                <img src={LogoTitle} alt="developer" />
+                asu,
+                <br />
+                Software
+                Engineering Student
+                </h1>
+                <h2> Tampere University of Applied Sciences </h2>
+                <Link to="/contact" className='flat-button'> Contact Me </Link>
             </div>
+
         </div>
     )
 };
